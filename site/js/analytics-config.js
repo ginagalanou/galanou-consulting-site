2a888bc6bf02926008dfd4f8f9781188b2751d82
ginagalanou <gina.galanou@gmail.com>
2026-09-18 23:06:58 +0300

    Update analytics-config.js

--- site/js/analytics-config.js
+++ site/js/analytics-config.js
@@ -2,8 +2,8 @@
 // and enhancedMeasurementDisabled after the GA4 stream's automatic events are off.
 // Never add preview hosts to the allowlist. Consent UI is visible for review.
 window.GALANOU_ANALYTICS = Object.freeze({
-  measurementId: '',
-  productionEnabled: false,
-  enhancedMeasurementDisabled: false,
+  measurementId: 'G-T62NN6N8ZP',
+  productionEnabled: true,
+  enhancedMeasurementDisabled: true,
   productionHosts: ['galanouconsulting.com', 'www.galanouconsulting.com']
 });
